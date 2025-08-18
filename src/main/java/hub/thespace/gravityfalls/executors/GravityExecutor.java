3383685b97d7c8776a85937124fd3410418725f5
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-18 15:31:08 +0300

    Изменение гравитации для всех в мире

--- src/main/java/hub/thespace/gravityfalls/executors/GravityExecutor.java
+++ src/main/java/hub/thespace/gravityfalls/executors/GravityExecutor.java
@@ -2,7 +2,10 @@ package hub.thespace.gravityfalls.executors;
 
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class GravityExecutor {
 
@@ -22,6 +25,23 @@ public class GravityExecutor {
         ConfigurationSection worlds = plugin.getConfig().getConfigurationSection("worlds");
         worlds.set(world.getName(), value);
         plugin.saveConfig();
+
+        updateWorld(world);
+    }
+
+    /**
+     * Обновить мир и применить новую гравитацию ко всем существам.
+     *
+     * @param world Мир.
+     */
+    public void updateWorld(World world) {
+        int gravity = plugin.getConfig().getConfigurationSection("worlds").getInt(world.getName());
+        for (LivingEntity entity : world.getLivingEntities()) {
+            entity.removePotionEffect(PotionEffectType.LEVITATION);
+            entity.addPotionEffect(new PotionEffect(
+                    PotionEffectType.LEVITATION,
+                    1000000, -gravity, false, false));
+        }
     }
 
 }
